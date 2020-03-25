cdeac2dad99f454b3b95cab59efc798070246312
Ankit <ankit.singh@clarivate.com>
2020-03-26 01:21:49 +0530

    Added and updated image resources

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -15,29 +16,39 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countButton: Button = findViewById(R.id.count_button)
-        countButton.setOnClickListener { countUp() }
+//        val countButton: Button = findViewById(R.id.count_button)
+//        countButton.setOnClickListener { countUp() }
     }
 
 
 
     private fun rollDice() {
         val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-    }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
 
-    private  fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
-        } else {
-            var resultInt = resultText.text.toString().toInt()
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        diceImage.setImageResource(drawableResource)
     }
+
+//    private  fun countUp() {
+//        val resultText: TextView = findViewById(R.id.result_text)
+//
+//        if (resultText.text == "Hello World!") {
+//            resultText.text = "1"
+//        } else {
+//            var resultInt = resultText.text.toString().toInt()
+//            if (resultInt < 6) {
+//                resultInt++
+//                resultText.text = resultInt.toString()
+//            }
+//        }
+//    }
 }
